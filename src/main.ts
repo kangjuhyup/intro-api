9483318c935815521aec5777b7bf87a855555a52
kangjuhyup <107104811+kangjuhyup@users.noreply.github.com>
2024-10-08 08:54:38 +0900

    feat : prod cors

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,13 @@ async function bootstrap() {
       allowedHeaders: 'Content-Type, Accept, Authorization', // 허용하는 요청 헤더
       credentials: true, // 쿠키 등 인증 관련 헤더 허용
     });
+  } else {
+    app.enableCors({
+      origin: 'https://introduce.jhkang.xyz', // React 앱 도메인
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // 허용하는 HTTP 메서드
+      allowedHeaders: 'Content-Type, Accept, Authorization', // 허용하는 요청 헤더
+      credentials: true, // 쿠키 등 인증 관련 헤더 허용
+    });
   }
   await app.listen(process.env.PORT || 3001);
 }
