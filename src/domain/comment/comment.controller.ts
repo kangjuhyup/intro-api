c1e95b6a948040c09c1c01f0c857d57f7764a6db
kangjuhyup <fog0510@gmail.com>
2024-10-12 16:56:23 +0900

    chore

--- src/domain/comment/comment.controller.ts
+++ src/domain/comment/comment.controller.ts
@@ -13,7 +13,7 @@ import { AddCommentRequest } from './dto/request/add.comment.request';
 import { GetCommentsRequest } from './dto/request/get.comments.request';
 import { HttpResponse } from 'src/dto/response.dto';
 import { GetCommentsResponse } from './dto/response/get.comments.response';
-import { interval, Observable, map, takeWhile } from 'rxjs';
+import { interval, Observable, map, takeWhile, switchMap, from } from 'rxjs';
 @Controller('comment')
 export class CommentController {
   private logger = new Logger(CommentController.name);
@@ -46,13 +46,20 @@ export class CommentController {
   }
 
   @Sse('observe')
-  commentSession(@Query('email') email: string): Observable<MessageEvent> {
+  commentSession(
+    @Query('email') email: string,
+  ): Observable<{ data: { confirm: boolean }; stop: boolean }> {
     return interval(1000).pipe(
-      map(() => {
-        const commentExists = this.commentService.getComment(email);
-        return { data: { confirm: !commentExists } };
-      }),
-      takeWhile(({ data }) => !data.confirm),
+      switchMap(() =>
+        from(this.commentService.getComment(email)).pipe(
+          map((comment) => {
+            if (!comment) return { data: { confirm: false }, stop: true }; // comment가 없으면 stop 플래그 설정
+            if (comment.verify) return { data: { confirm: true }, stop: true }; // verify가 true면 스트림 종료 플래그 설정
+            return { data: { confirm: false }, stop: false }; // 아직 verify되지 않은 경우
+          }),
+        ),
+      ),
+      takeWhile(({ stop }) => !stop, true), // stop 플래그가 true가 될 때까지 스트림 유지
     );
   }
 }
